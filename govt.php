69943d89c39ad5a0fe70ff42b3ca889681d4e307
Shaharyaar7 <shaharyaarkutchi7@gmail.com>
2021-05-17 22:03:13 +0530

    created modules

---
+++ govt.php
@@ -0,0 +1,108 @@
+<?php include('security.php'); ?>
+<?php include('includes/header.php');?>
+<?php include('includes/navbar.php');?>
+
+
+<br><br><br><br>
+
+  
+<style type="text/css">
+  
+.button{
+  background: #3498db;
+    color: #fff;
+    border-radius: 50px;
+    margin: 0 15px;
+    padding: 10px 30px; 
+}
+
+.services .icon-box {
+    padding: 60px 30px;
+    transition: all ease-in-out 0.3s;
+    background: #fefefe;
+    box-shadow: 0px 5px 90px 0px rgba(110, 123, 131, 0.1);
+    border-radius: 18px;
+    border-bottom: 5px solid #fff;
+    height: 150px;
+    width: 400px;
+}
+  
+</style>
+    <!-- bradcam_area_start  -->
+    <div class="bradcam_area breadcam_bg ">
+        <div class="container">
+          <br><br>
+            <div class="row">
+                <div class="col-lg-6">
+                    <div class="bradcam_text">
+                        <img src="img/jharkhand/logo.png" class="img-fluid" style="height: 150px;">
+                        <h1>Government Services</h1>
+                        <p>Helpline number for COVID-19 0651 2490037, 0651 2490055,<br> 0651 2490052, 0651 2490058, 0651 2490083.<br>
+                        <a href="https://www.jharkhand.gov.in/Home/G2P"> Citizen Centric Link List</a>  
+                        </p>
+                    </div>
+                </div>
+                <div class="col-lg-6">
+                    <div class="bradcam_text">
+                      <br><br><br>
+                        <img src="img/jharkhand/banner.jpg" class="img-fluid">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+     <!-- ======= Services Section ======= -->
+    <section id="services" class="services">
+      <div class="container">
+
+        <div class="section-title">
+          <h2>E-Services</h2>
+        </div>
+
+        <div class="row">
+          <div class="col-lg-4 col-md-6 d-flex align-items-stretch">
+            <div class="icon-box">
+              <h4><a href="https://jharsewa.jharkhand.gov.in/">JharSewa</a></h4>
+            </div>
+          </div>
+
+          <div class="col-lg-4 col-md-6 d-flex align-items-stretch mt-4 mt-md-0">
+            <div class="icon-box">
+              <h4><a href="http://enibandhan.jharkhand.gov.in/"> e-Nibandhan</a></h4>
+            </div>
+          </div>
+
+          <div class="col-lg-4 col-md-6 d-flex align-items-stretch mt-4 mt-lg-0">
+            <div class="icon-box">
+              <h4><a href="http://jharkhandcomtax.gov.in/gst"> Jharkhand VAT (Non-GST)</a></h4>
+            </div>
+          </div>
+
+          <div class="col-lg-4 col-md-6 d-flex align-items-stretch mt-4">
+            <div class="icon-box">
+              <h4><a href="https://digitallocker.gov.in/"> DigiLocker</a></h4>
+            </div>
+          </div>
+
+          <div class="col-lg-4 col-md-6 d-flex align-items-stretch mt-4">
+            <div class="icon-box">
+              <h4><a href="http://aahar.jharkhand.gov.in/"> Aahar Jharkhand</a></h4>
+            </div>
+          </div>
+
+          <div class="col-lg-4 col-md-6 d-flex align-items-stretch mt-4">
+            <div class="icon-box">
+             <h4><a href="https://portal2.passportindia.gov.in/">  Passport</a></h4>
+            </div>
+          </div>
+
+        </div>
+
+      </div>
+    </section><!-- End Services Section -->
+
+
+
+<?php include('includes/scripts.php');?>
+<?php include('includes/footer.php');?>
---
+++ index.php
@@ -0,0 +1,315 @@
+<?php include('security.php'); ?>
+<?php include('includes/header.php');?>
+<?php include('includes/navbar.php');?>
+
+
+  <!-- ======= Hero Section ======= -->
+  <section id="hero" class="d-flex flex-column justify-content-center">
+    
+  </section><!-- End Hero -->
+
+  <main id="main">
+
+    <!-- ======= Featured Services Section ======= -->
+  <!--   <section id="featured-services" class="featured-services">
+      <div class="container" data-aos="fade-up">
+
+        <div class="row">
+          <div class="col-md-6 col-lg-3 d-flex align-items-stretch mb-5 mb-lg-0">
+            <div class="icon-box" data-aos="fade-up" data-aos-delay="100">
+              <div class="icon"><img src="img/icons/virus.png"></div>
+              <h4 class="title" id="StateConfirmed"></h4>
+              <p class="description" >Confirmed Cases</p>
+            </div>
+          </div>
+
+           <div class="col-md-6 col-lg-3 d-flex align-items-stretch mb-5 mb-lg-0">
+            <div class="icon-box" data-aos="fade-up" data-aos-delay="100">
+              <div class="icon"><img src="img/icons/virus.png"></div>
+              <h4 class="title" id="StateActive"></h4>
+              <p class="description" >Active Cases</p>
+            </div>
+          </div>
+
+           <div class="col-md-6 col-lg-3 d-flex align-items-stretch mb-5 mb-lg-0">
+            <div class="icon-box" data-aos="fade-up" data-aos-delay="100">
+              <div class="icon"><img src="img/icons/virus.png"></div>
+              <h4 class="title" id="StateRecovered"></h4>
+              <p class="description" >Recovered Cases</p>
+            </div>
+          </div>
+
+           <div class="col-md-6 col-lg-3 d-flex align-items-stretch mb-5 mb-lg-0">
+            <div class="icon-box" data-aos="fade-up" data-aos-delay="100">
+              <div class="icon"><img src="img/icons/virus.png"></div>
+              <h4 class="title" id="StateDeath"></h4>
+              <p class="description" >Death Cases</p>
+            </div>
+          </div>
+
+        </div>
+
+      </div>
+    </section> --><!-- End Featured Services Section -->
+
+
+    <!-- ======= About Us Section ======= -->
+    <section id="about" class="about">
+      <div class="container" data-aos="fade-up">
+
+        <div class="section-title">
+          <h2>About Us</h2>
+        </div>
+
+        <div class="row">
+          <div class="col-lg-6" data-aos="fade-right">
+            <img src="img/jharkhand/map.jpg" class="img-fluid" alt="" style="height: 450px; width: 400px;">
+          </div>
+          <div class="col-lg-6 pt-4 pt-lg-0 content" data-aos="fade-left">
+            <h3> Jharkhand</h3>
+            <p class="font-italic">
+              Jharkhand is a state in eastern India. It's known for its waterfalls, the elegant Jain temples of Parasnath Hill and the elephants and tigers of Betla National Park. The state capital of Ranchi is a gateway to the park. It features the 17th-century Jagannath Temple, a Hindu shrine and the Jharkhand War Memorial. Tagore Hill is a monument honoring Nobel Prize-winning author Rabindranath Tagore
+            </p>
+            <ul>
+              <li><i class="icofont-check-circled"></i><strong>Capital:</strong> Ranchi</li>
+              <li><i class="icofont-check-circled"></i><strong>Population:</strong> 3.19 crores (2012)</li>
+              <li><i class="icofont-check-circled"></i><strong>Chief minister:</strong> Hemant Soren
+              </li>
+              <li><i class="icofont-check-circled"></i><strong>Literacy Rate:</strong> 67.6% (31st)</li>
+            </ul>
+            
+          </div>
+        </div>
+
+      </div>
+    </section><!-- End About Us Section -->
+
+    <!-- ======= Cta Section ======= -->
+    <section id="cta" class="cta">
+      <div class="container" data-aos="zoom-in">
+
+        <div class="text-center">
+          <h3>Coronavirus</h3>
+          <p>Coronavirus disease (COVID-19) is an infectious disease caused by a newly discovered coronavirus.
+Most people infected with the COVID-19 virus will experience mild to moderate respiratory illness and recover without requiring special treatment.Older people, and those with underlying medical problems like cardiovascular disease, diabetes, chronic respiratory disease, and cancer are more likely to develop serious illness.<br><br>
+The COVID-19 virus spreads primarily through droplets of saliva or discharge from the nose when an infected person coughs or sneezes, so it’s important that you also practice respiratory etiquette (for example, by coughing into a flexed elbow).
+At this time, there are no specific vaccines or treatments for COVID-19. However, there are many ongoing clinical trials evaluating potential treatments. WHO will continue to provide updated information as soon as clinical findings become available.</p>
+        </div>
+
+      </div>
+    </section><!-- End Cta Section -->
+
+    <!-- ======= Counts Section ======= -->
+    <section id="counts" class="counts">
+      <div class="container" data-aos="fade-up">
+
+        <div class="row no-gutters">
+
+          <div class="col-lg-3 col-md-6 d-md-flex align-items-md-stretch">
+            <div class="count-box">
+              <img src="img/icons/virus.png">
+              <span data-toggle="counter-up" id="StateConfirmed"></span>
+              <p><strong>Confirmed Cases</strong> </p>
+            </div>
+          </div>
+
+          <div class="col-lg-3 col-md-6 d-md-flex align-items-md-stretch">
+            <div class="count-box">
+              <img src="img/icons/virus.png">
+              <span data-toggle="counter-up" id="StateActive"></span>
+              <p><strong>Active Cases</strong></p>
+            </div>
+          </div>
+
+          <div class="col-lg-3 col-md-6 d-md-flex align-items-md-stretch">
+            <div class="count-box">
+              <img src="img/icons/virus.png">
+              <span data-toggle="counter-up" id="StateRecovered"></span>
+              <p><strong>Recovered Cases</strong></p>
+            </div>
+          </div>
+
+          <div class="col-lg-3 col-md-6 d-md-flex align-items-md-stretch">
+            <div class="count-box">
+              <img src="img/icons/virus.png">
+              <span data-toggle="counter-up" id="StateDeath"></span>
+              <p><strong>Death Cases</strong></p>
+            </div>
+          </div>
+
+        </div>
+
+      </div>
+    </section><!-- End Counts Section -->
+
+     <!-- ======= Features Section ======= -->
+    <section id="features" class="features">
+      <div class="container">
+        <h2>To Flatten The Curve & Break the Chain</h2>
+          <p>STAY HOME, STAY SAFE</p>
+        <div class="row">
+          <div class="col-lg-4 order-2 order-lg-1">
+            <div class="icon-box mt-5 mt-lg-0">
+              <i class="bx bx-shield"></i>
+              <h4>KEEP A SAFE DISTANCE</h4>
+            </div>
+            <div class="icon-box mt-5">
+              <i class="bx bx-shield"></i>
+              <h4>WASH HANDS OFTEN</h4>
+            </div>
+            <div class="icon-box mt-5">
+              <i class="bx bx-shield"></i>
+              <h4>COVER YOUR COUGH</h4>
+            </div>
+            <div class="icon-box mt-5">
+              <i class="bx bx-shield"></i>
+              <h4>SICK? CALL THE HELPLINE</h4>
+            </div>
+          </div>
+          <div class="image col-lg-8 order-1 order-lg-2 img-fluid" style='background-image: url("img/home/banner.jpg");'></div>
+        </div>
+
+      </div>
+    </section><!-- End Features Section -->
+
+
+    <!-- ======= Services Section ======= -->
+    <section id="services" class="services services">
+      <div class="container">
+
+        <div class="section-title">
+          <h2>Coronavirus Symptoms</h2>
+        </div>
+
+        <div class="row">
+          <div class="col-lg-4 col-md-6 icon-box d-flex align-items-stretch">
+            <div class="icon-box">
+              <div class="icon"><img src="img/icons/cold.png" style="height: 42px;"></div>
+              <h4 class="title" ><a href="#">Fever</a></h4>
+              <p class="description">A fever is a body temperature that is higher than normal. A normal temperature can vary from person to person,but it is usually around 98.6F.</p>
+            </div>
+          </div>
+
+          <div class="col-lg-4 col-md-6 icon-box d-flex align-items-stretch mt-4 mt-md-0">
+            <div class="icon-box">
+              <div class="icon"><img src="img/icons/education.png" style="height: 42px;"></div>
+              <h4 class="title" ><a href="#">Tiredness</a></h4>
+              <p class="description">Feeling overtired, with low energy and a strong desire to sleep that interferes with normal daily activities.</p>
+            </div>
+          </div>
+
+          <div class="col-lg-4 col-md-6 icon-box d-flex align-items-stretch mt-4 mt-lg-0">
+            <div class="icon-box">
+              <div class="icon"><img src="img/icons/avatar.png" style="height: 42px;"></div>
+              <h4 class="title" ><a href="#">Dry Cough</a></h4>
+              <p class="description">A sudden, forceful hacking sound to release air and clear an irritation in the throat or airway.</p>
+            </div>
+          </div>
+
+          <div class="col-lg-4 col-md-6 icon-box d-flex align-items-stretch mt-4">
+            <div class="icon-box">
+              <div class="icon"><img src="img/icons/healthcare-and-medical (2).png" style="height: 42px;"></div>
+              <h4 class="title" ><a href="#">Aches and Pains</a></h4>
+              <p class="description">Muscle pain is most often related to tension, overuse, or muscle injury from exercise or hard physical work.</p>
+            </div>
+          </div>
+
+          <div class="col-lg-4 col-md-6 icon-box d-flex align-items-stretch mt-4">
+            <div class="icon-box">
+              <div class="icon"><img src="img/icons/discharge.png" style="height: 42px;"></div>
+              <h4 class="title" ><a href="#">Nasal Congestion</a></h4>
+              <p class="description">Nasal passages swollen with excess fluid and mucus, may be triggered by infection, tobacco smoke or perfume.</p>
+            </div>
+          </div>
+
+          <div class="col-lg-4 col-md-6 icon-box d-flex align-items-stretch mt-4">
+            <div class="icon-box">
+              <div class="icon"><img src="img/icons/discharge (5).png" style="height: 42px;"></div>
+             <h4 class="title" ><a href="#">Runny Nose</a></h4>
+              <p class="description">Excess drainage, ranging from a clear fluid to thick mucus, from the nose and nasal passages.</p>
+            </div>
+          </div>
+
+        </div>
+
+      </div>
+    </section><!-- End Services Section -->
+
+    <!-- ======= Gallery Section ======= -->
+    <section id="gallery" class="gallery">
+      <div class="container" data-aos="fade-up">
+
+        <div class="section-title">
+          <h2>Hamar Jharkhand Services</h2>
+        </div>
+
+        <div class="owl-carousel gallery-carousel" data-aos="fade-up" data-aos-delay="100">
+          <a href="img/home/center.jpg" class="venobox" data-gall="gallery-carousel" ><img src="img/home/center.jpg" alt=""></a>
+          <a href="img/home/medical.png" class="venobox" data-gall="gallery-carousel" ><img src="img/home/medical.png" alt=""></a>
+          <a href="img/home/blood.png" class="venobox" data-gall="gallery-carousel" ><img src="img/home/blood.png" alt=""></a>
+          <a href="img/home/donate.png" class="venobox" data-gall="gallery-carousel" ><img src="img/home/donate.png" alt=""></a>
+          <a href="img/home/volunteer.jpg" class="venobox" data-gall="gallery-carousel"><img src="img/home/volunteer.jpg" alt=""></a>
+          <a href="img/jharkhand/logo.jpeg" class="venobox" data-gall="gallery-carousel" ><img src="img/jharkhand/logo.jpeg" alt=""></a>
+          <a href="img/home/mask.jpg" class="venobox" data-gall="gallery-carousel" ><img src="img/home/mask.jpg" alt=""></a>
+          <a href="img/home/hr.png" class="venobox" data-gall="gallery-carousel" ><img src="img/home/hr.png" alt=""></a>
+
+          <a href="img/home/volunteer.jpg" class="venobox" data-gall="gallery-carousel" ><img src="img/home/volunteer.jpg" alt=""></a>
+          <a href="img/home/donation.jpg" class="venobox" data-gall="gallery-carousel" ><img src="img/home/donation.jpg" alt=""></a>
+          <a href="img/home/msme.jpg" class="venobox" data-gall="gallery-carousel" ><img src="img/home/msme.jpg" alt=""></a>
+          <a href="img/home/courses.png" class="venobox" data-gall="gallery-carousel" ><img src="img/home/courses.png" alt=""></a>
+        </div>
+
+      </div>
+    </section><!-- End Gallery Section -->
+
+
+  </main><!-- End #main -->
+
+<script type="text/javascript">
+  
+$.ajax({
+    url:"https://api.covid19india.org/v2/state_district_wise.json",
+    type:"GET",
+    dataType:"JSON",
+    success : function(covid_data) {
+
+     //console.log(covid_data);
+     var newArray = covid_data.filter(function (el) {
+
+      return el.state === 'Jharkhand';
+
+    });
+
+    //console.log(newArray);
+
+    Array.prototype.sum = function (prop) {
+    var total = 0
+    for ( var i = 0, _len = this.length; i < _len; i++ ) {
+        total += this[i][prop]
+    }
+    return total
+    }
+
+    var StateConfirmed = newArray[0].districtData.sum("confirmed");
+    document.getElementById("StateConfirmed").innerHTML = StateConfirmed;
+
+    var StateActive = newArray[0].districtData.sum("active");
+    document.getElementById("StateActive").innerHTML = StateActive;
+
+    var StateDeath = newArray[0].districtData.sum("deceased");
+    document.getElementById("StateDeath").innerHTML = StateDeath;
+
+    var StateRecovered = newArray[0].districtData.sum("recovered");
+    document.getElementById("StateRecovered").innerHTML = StateRecovered;
+
+    // var MainStateConfirmed = newArray[0].districtData.sum("confirmed");
+    // document.getElementById("MainStateConfirmed").innerHTML = "Covid19 Cases - " + (MainStateConfirmed) ;
+}
+
+});
+
+
+</script>
+
+<?php include('includes/scripts.php');?>
+<?php include('includes/footer.php');?>
